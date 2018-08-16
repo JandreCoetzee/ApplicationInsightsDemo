f3a4469c9e7fce599669f38039a2b2dd8d3d1f6f
Jandre' Coetzee <jandre.coetzee@entelect.co.za>
2018-08-16 19:39:34 +0200

    Adde DotNet.API project

---
+++ src/DotNet/DotNet.API/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Http;
+
+namespace DotNet.API.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        [Route("values")]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                this.GetValue();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        private string GetValue()
+        {
+            throw new NotImplementedException("This endpoint has not yet been implemented.");
+        }
+    }
+}
---
+++ src/DotNet/DotNet.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNet.API.WebApiApplication" Language="C#" %>
